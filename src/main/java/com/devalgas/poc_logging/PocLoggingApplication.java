4b9fa324b441a7a97731d6f44224773d61998c6a
devalgas <kamgadevalgas@gmail.com>
2025-01-29 09:17:31 +0100

    first commit

---
+++ src/main/java/com/devalgas/poc_logging/PocLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.devalgas.poc_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devalgas/poc_logging/PocLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalgas.poc_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
